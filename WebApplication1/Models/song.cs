8975693edb81443b1cdd8f1a072b5a7c33136918
Lester Chavez <lchavez9669@conestogac.on.ca>
2017-10-15 23:38:30 -0400

    song and song list class completed.

---
+++ WebApplication1/Models/song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Song
+    {
+        private int id;
+        private string songName;
+    
+        
+        public Song()
+        {
+
+        }
+        public Song(int ID,string SongName)
+        {
+            id = ID;
+            songName = SongName;
+        }
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string SongName
+        {
+            get { return songName; }
+            set { songName = value; }
+        }
+    }
+
+}
---
+++ WebApplication1/Models/songList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class SongList
+    {
+        private static List<Song> songs = new List<Song>()
+        {
+            new Song {ID=1, SongName="Moonlign Sonata"},
+            new Song {ID=2, SongName="Put Your Head On My Shoulder"},
+            new Song {ID=3, SongName="Earth Angel"}
+
+
+        };
+        public static void AddSongName(Song newSong)
+        {
+            songs.Add(newSong);
+        }
+
+        public static List<Song> GetAllSongs()
+        {
+            return songs;
+        }
+
+        public static Song GetASong(int id)
+        {
+            Song r = new Song();
+            foreach (Song s in songs)
+                if (s.ID == id)
+                    r = s;
+            return r;
+        }
+
+        //public static bool Update(int id, Song UpdateSong)
+        //{
+        //    bool returnValue = false;
+
+        //    foreach (Song s in songs)
+        //        if (s.ID == id)
+        //    {
+        //            s.SongName = UpdateSong.SongName;
+        //            returnValue = true;
+        //    }
+        //    return returnValue;
+        //}
+    }
+}
